aba902d036323eacd0a9e443b8a560b1d4281424
VPS-JLueken <dart@vansd.org>
2023-09-15 15:16:31 -0700

    Daily Commit

--- Day_002 - Beginner - Understanding Data Types and How to Manipulate Strings/Python Primitive Data Types.py
+++ Day_002 - Beginner - Understanding Data Types and How to Manipulate Strings/Python Primitive Data Types.py
@@ -6,3 +6,20 @@
 "Hello"[0] #returns the index of the character based on the number
 print("Hello"[0])
 print("Hello"[4])
+
+
+#Integer - Whole numbers
+
+print(123 + 345)
+
+123_456_789 # this is used to visualize large numbers like 123,456,789
+
+
+#Float - floating point number
+
+3.14159
+
+
+#Boolean
+
+True or False #Always start with capitol T and F
---
+++ Day_002 - Beginner - Understanding Data Types and How to Manipulate Strings/Type Error, Tpe Checking and Type Conversion.py
@@ -0,0 +1,20 @@
+# #len(4837) #This will return a type error
+#
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)  # Type casting
+#
+# # print("your name has " + num_char + " characters.") #throws a TypeError, can only concatenate str (not "int") to str
+#
+# print("your name has " + new_num_char + " characters.")
+#
+# print(type(num_char)) # Type checking
+# print(type(new_num_char))
+
+
+# a = str(123) #converts int to string
+a = float(123)
+print(type(a))
+
+print(70 + float(100.5))
+
+print(str(70)+ str(100))
