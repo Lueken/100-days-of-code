bda2f0af8293b4c0d346bbd1e5cc350ce789c268
VPS-JLueken <dart@vansd.org>
2023-09-18 15:17:56 -0700

    daily commit

---
+++ Day_002 - Beginner - Understanding Data Types and How to Manipulate Strings/Exercise - BMI Calculator.py
@@ -0,0 +1,12 @@
+# Don't change the code below
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# Don't change the code above
+
+#Write your code below this line
+
+# BMI = weight / height squared
+
+bmi = int(weight)/float(height)**2
+
+print(bmi)
