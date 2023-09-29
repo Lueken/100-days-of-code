5e36ea9190bdc9a0c6baf5408848379d6e22584b
VPS-JLueken <dart@vansd.org>
2023-09-29 14:59:51 -0700

    day 6 start

---
+++ Day_006 - Beginner - Python Functions & Karel/Defining and Calling Python Functions.py
@@ -0,0 +1,20 @@
+# Functions
+
+
+#Built-in Functions
+print("Hello") # Functions are followed by ()
+num_char = len("Hello")
+print(num_char)
+
+#Hand made Function
+
+def my_function(): #everything after, and indented, defines this function
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# def my_function():
+    #Do this
+    #Then do this
+    #Finally do this
